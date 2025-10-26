2435573305e8c7b2346cc420663cf6afb92527af
AD <silentflute.ad@gmail.com>
2025-10-26 17:52:48 +0000

    Recreating package file, from the ground up

---
+++ Werks/Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Werks",
+    platforms: [
+            .macOS(.v10_15),
+            .iOS(.v15),
+            .watchOS(.v6),
+            .tvOS(.v13),
+        ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Werks",
+            targets: ["Werks"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Werks"),
+        .testTarget(
+            name: "WerksTests",
+            dependencies: ["Werks"]
+        ),
+    ]
+)
