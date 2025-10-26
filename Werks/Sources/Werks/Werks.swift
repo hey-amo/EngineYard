00460ee45520aa159ebabf489f21730a31e7169b
AD <silentflute.ad@gmail.com>
2025-10-26 18:41:53 +0000

    Adding a bunch of models

--- Werks/Sources/Werks/Werks.swift
+++ Werks/Sources/Werks/Werks.swift
@@ -50,12 +50,40 @@
  5. Market Demands
  Players determine Market Demands. Subsequently the next game round starts.
 
+ ---
+ 
+ # 🚂 Train Unit Data Table
+
+ | **ID** | **Name** | **Colour** | **Cost** | **Colour Code** | **Gen** | **Pool** | **Dice** |
+ |:------:|:----------------------------|:-----------|:--------:|:----------------:|:-------:|:--------:|:--------:|
+ | 1  | General I (4-2-1)          | Green  | 4  | 1 | 1 | 4 | 3 |
+ | 2  | Fast Freight I (8-4-2)     | Red    | 8  | 2 | 1 | 3 | 3 |
+ | 3  | Heavy I (12-6-3)           | Yellow | 12 | 3 | 1 | 2 | 2 |
+ | 4  | Special I (16-8-4)         | Blue   | 16 | 4 | 1 | 1 | 2 |
+ | 5  | General II (20-10-5)       | Green  | 20 | 1 | 2 | 4 | 4 |
+ | 6  | Fast Freight II (24-12-6)  | Red    | 24 | 2 | 2 | 3 | 3 |
+ | 7  | Heavy II (28-14-7)         | Yellow | 28 | 3 | 2 | 2 | 3 |
+ | 8  | General III (32-16-8)      | Green  | 32 | 1 | 3 | 4 | 4 |
+ | 9  | Special II (36-18-9)       | Blue   | 36 | 4 | 2 | 2 | 2 |
+ | 10 | Fast Freight III (40-20-10)| Red    | 40 | 2 | 3 | 3 | 4 |
+ | 11 | General IV (44-22-11)      | Green  | 44 | 1 | 4 | 4 | 4 |
+ | 12 | Heavy III (48-24-12)       | Yellow | 48 | 3 | 3 | 3 | 3 |
+ | 13 | Fast Freight IV (52-26-13) | Red    | 52 | 2 | 4 | 4 | 4 |
+ | 14 | General V (56-28-14)       | Green  | 56 | 1 | 5 | 4 | 5 |
+
+ 
  ------------------------------------
  */
 
+import Foundation
+
+// ------------------------------------
+
 // MARK: Locomotive Color
 
-public enum LocomotiveColor: Int, CaseIterable, Equatable, Codable {
+public typealias Livery = LocomotiveColor
+
+public enum LocomotiveColor: Int, CaseIterable, Equatable {
     case green = 1, red, yellow, blue
 }
 
@@ -77,7 +105,7 @@ extension LocomotiveColor: CustomStringConvertible {
 
 public typealias Generation = LocomotiveGeneration
 
-public enum LocomotiveGeneration: Int, CaseIterable, Codable {
+public enum LocomotiveGeneration: Int, CaseIterable {
     case first = 1, second, third, fourth, fifth
 }
 
@@ -104,3 +132,140 @@ extension LocomotiveGeneration: Equatable {
         return (lhs.rawValue == rhs.rawValue)
     }
 }
+
+// ------------------------------------
+
+public enum OrderState: Int, CaseIterable {
+    case empty, initialOrder, existingOrder, filledOrder, invalid
+}
+
+/**
+Game stages:
+ 
+ 1. Locomotive Development
+ Each player may develop 1 new locomotive.
+ 
+ 2. Production Capacity
+ Each player may expand his production capacities.
+ 
+ 3. Locomotive Production
+ Each player may produce and sell locomotives.
+ 
+ 4. Pay Taxes
+ Each player must pay taxes.
+ If at least one player has 300 coins or more after paying taxes, then the game ends; otherwise determine the new Player
+ Order.
+
+ 5. Market Demands
+ */
+public enum WerksGameStage: Int, CaseIterable {
+    case idle
+    case gameSetup
+    case locomotiveDevelopment
+    case expandProductionCapacity
+    case produceAndSellLocomotives
+    case payTaxes
+    case marketDemands
+    case gameOver
+}
+
+public protocol GameBoardSpace {
+    var position: Int { get }
+    var locomotiveID: Int { get }
+    var existingOrders: [Int] { get set }
+    var filledOrders: [Int] { get set }
+    var initialOrder: Int { get set }
+    var orderState: OrderState { get set }
+    var diceCap: Int { get }
+}
+
+public protocol LocomotiveData {
+    var id: Int { get } // Unique ref of the locomotive
+    var name: String { get } // Name of the locomotive
+    var color: Livery { get } // Locomotive.color
+    var generation: Generation { get } // Locomotive.generation
+    var cost: Int { get } // Cost to design the locomotive
+    var trainPool: Int { get set } // How many of these locomotives exist
+    var maxDice: Int { get } // How many dice can be assigned to this
+}
+
+// ------------------------------------
+
+// MARK: GameBoard
+
+// The game board is made up of 14 spaces
+public class GameBoard {
+    private let _spaces: [GameBoardSpace]
+    public var spaces: [GameBoardSpace] {
+        get {
+            return self._spaces
+        }
+    }
+    
+    init(_spaces: [GameBoardSpace]) {
+        self._spaces = _spaces
+    }
+}
+
+// ------------------------------------
+
+// MARK: Locomotive
+
+public class Locomotive: LocomotiveData, Identifiable, Hashable, Equatable {
+    public var id: Int
+    public var name: String
+    public var color: Livery
+    public var generation: Generation
+    public var cost: Int
+    public var trainPool: Int
+    public var maxDice: Int
+    
+    init(id: Int, name: String, color: Livery, generation: Generation, cost: Int, trainPool: Int, maxDice: Int) {
+        self.id = id
+        self.name = name
+        self.color = color
+        self.generation = generation
+        self.cost = cost
+        self.trainPool = trainPool
+        self.maxDice = maxDice
+    }
+    
+    public static func == (left: Locomotive, right: Locomotive) -> Bool {
+        return (left.id == right.id)
+    }
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+        hasher.combine(color)
+        hasher.combine(generation)
+    }
+}
+
+// ------------------------------------
+
+extension Locomotive {
+    public func buildLocomotives() -> [Locomotive] {
+        // An array of all locomotives in the game (immutable)
+        
+        let locos: [Locomotive] = [
+            Locomotive(id: 1, name: "General I (4-2-1)", color: .green, generation: .first, cost: 4, trainPool: 4, maxDice: 3),
+            Locomotive(id: 2, name: "Fast Freight I (8-4-2)", color: .red, generation: .first, cost: 8, trainPool: 3, maxDice: 3),
+            Locomotive(id: 3, name: "Heavy I (12-6-3)", color: .yellow, generation: .first, cost: 12, trainPool: 2, maxDice: 2),
+            Locomotive(id: 4, name: "Special I (16-8-4)", color: .blue, generation: .first, cost: 16, trainPool: 1, maxDice: 2),
+            Locomotive(id: 5, name: "General II (20-10-5)", color: .green, generation: .second, cost: 20, trainPool: 4, maxDice: 4),
+            Locomotive(id: 6, name: "Fast Freight II (24-12-6)", color: .red, generation: .second, cost: 24, trainPool: 3, maxDice: 3),
+            Locomotive(id: 7, name: "Heavy II (28-14-7)", color: .yellow, generation: .second, cost: 28, trainPool: 2, maxDice: 3),
+            Locomotive(id: 8, name: "General III (32-16-8)", color: .green, generation: .third, cost: 32, trainPool: 4, maxDice: 4),
+            Locomotive(id: 9, name: "Special II (36-18-9)", color: .blue, generation: .second, cost: 36, trainPool: 2, maxDice: 2),
+            Locomotive(id: 10, name: "Fast Freight III (40-20-10)", color: .red, generation: .third, cost: 40, trainPool: 3, maxDice: 4),
+            Locomotive(id: 11, name: "General IV (44-22-11)", color: .green, generation: .fourth, cost: 44, trainPool: 4, maxDice: 4),
+            Locomotive(id: 12, name: "Heavy III (48-24-12)", color: .yellow, generation: .third, cost: 48, trainPool: 3, maxDice: 3),
+            Locomotive(id: 13, name: "Fast Freight IV (52-26-13)", color: .red, generation: .fourth, cost: 52, trainPool: 4, maxDice: 4),
+            Locomotive(id: 14, name: "General V (56-28-14)", color: .green, generation: .fifth, cost: 56, trainPool: 4, maxDice: 5)
+        ].sorted { $0.cost < $1.cost }
+        
+        return locos
+    }
+}
+
+// ------------------------------------
