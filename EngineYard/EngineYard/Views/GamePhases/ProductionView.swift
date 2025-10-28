eaa38309996022c9de64c8bb233e03f879d471b2
AD <silentflute.ad@gmail.com>
2025-10-28 12:52:18 +0000

    Adding more dummy pages Adding a folder to contain game phases

---
+++ EngineYard/EngineYard/Views/GamePhases/ProductionView.swift
@@ -0,0 +1,39 @@
+//
+//  ProductionView.swift
+//  EngineYard
+//
+//  Created by Amarjit on 28/10/2025.
+//
+
+import SwiftUI
+
+/*
+ Phases:
+ 1. Locomotive Development
+ Each player may develop 1 new locomotive.
+
+ 2. Production Capacity
+ Each player may expand his production capacities.
+
+ 3. Locomotive Production
+ Each player may produce and sell locomotives.
+
+ 4. Pay Taxes
+ Each player must pay taxes.
+ If at least one player has 300 coins or more after paying taxes, then the game ends; otherwise determine the new Player
+ Order.
+
+ 5. Market Demands
+ Players determine Market Demands. Subsequently the next game round starts.
+ 
+ */
+
+struct ProductionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductionView()
+}
