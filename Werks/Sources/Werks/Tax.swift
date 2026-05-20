4e72aa0d237b4958d79f05dc2880c6f40b06c0ba
AD <silentflute.ad@gmail.com>
2026-05-20 07:09:39 +0100

    Adding itmes

--- Werks/Sources/Werks/Tax.swift
+++ Werks/Sources/Werks/Tax.swift
@@ -6,10 +6,10 @@
 //
 
 public struct Tax {
-    public func pay() {
-        
-    }
-    public func calculate() {
-        
-    }
+    /*
+    public static func calculateTax(on amount: Int) -> Int {
+        let taxRate: Double = GameRules.Economy.taxRate
+        guard (amount > 0) else { return 0 }
+        return Int(floor(Double(amount) * taxRate))
+    }*/
 }
--- Werks/Sources/Werks/Werks.swift
+++ Werks/Sources/Werks/Werks.swift
@@ -1,6 +1,95 @@
 // Werks
 // v0.1a
 
+// Main game engine for unofficial clone of `Locomotive Werks`.
+
+/**
+ # 🚂  Werks
+ - 3 to 5 players
+ - Most money wins
+ 
+ # Components:
+ - 1 game board
+    - 14 board spaces (linear sequence)
+        1. Green: 1st Gen, Cost: $4, Production: 2, Income: 1, 3 dice boxes
+        2. Red: 1st Gen, Cost: $8, Prod: 4, Income: 2, 3 dice boxes)
+        3. Yellow: 1st Gen, Cost: $12, Prod: 6, Income: 3, 2 dice boxes
+        4. Blue: 1st Gen, Cost: $16, Prod: 8, Income: 4, 1 dice boxes
+        5. Green: 2nd Gen, Cost: $20, Prod: 10, Income: 5, 4 dice boxes
+        6. Red: 2nd Gen, Cost: $24, Prod: 12, Income: 6, 3 dice boxes
+        7. Yellow: 2nd Gen, Cost: $28, Prod: 14, Income: 7, 3 dice boxes
+        8. Green: 3rd Gen,  Cost: $32, Prod: 16, Income: 8, 4 dice boxes
+        9. Blue: 2nd Gen, Cost: $36, Prod: 18, Income: 9, 2 dice boxes
+        10. Red: 3rd Gen, Cost: $40, Prod: 20, Income: 10, 4 dice boxes
+        11. Green: 4th Gen, Cost: $44, Prod: 22, Income: 11, 5 dice boxes
+        12, Yellow: 3rd Gen, Cost: $48, Prod: 24, Income: 12,  3 dice boxes
+        13, Red: 4th Gen, Cost: $52, Prod: 26, Income: 13, 4 dice boxes
+        14, Green: 5th Gen, Cost: $56, Prod: 28, Income: 14, 5 dice boxes
+ - 43 locomotive cards
+    - 20 Passenger locomotives (green)
+        - (4 First Generation, 4 Second Generation, 4 Third Generation, 4 Fourth Generation, 4 Fifth Generation)
+    - 13 Fast locomotives (red)
+        - (3 First Generation, 3 Second Generation, 3 Third Generation, 4 Fourth Generation)
+    - 7 Freight locomotives (yellow)
+        - (2 First Generation, 2 Second Generation, 3 Third Generation)
+    - 3 Special locomotives (blue)
+        - 1 First Generation, 2 Second Generation)
+ - 5 Turn order cards (1-5)
+ - 80 Production counters (Value: 1 or 2)
+ - Money/Coins
+ 
+ # Objective of the game
+
+ Players assume the role of engineers, constructing and producing more and more advanced locomotives.
+ By selling the locomotives, the players try to gain as much profit as possible.
+ 
+ When the game ends the player with the most money wins.
+ 
+ Werks is played in rounds.
+ 
+ Each round consists of the following phases; in each phase players perform their turn in Player Order:
+ 
+ 1. Locomotive Development
+ Each player may develop 1 new locomotive.
+ 
+ 2. Production Capacity
+ Each player may expand his production capacities.
+ 
+ 3. Locomotive Production
+ Each player may produce and sell locomotives.
+ 
+ 4. Pay Taxes
+ Each player must pay taxes.
+ If at least one player has 300 coins or more after paying taxes, then the game ends; otherwise determine the new Player
+ Order.
+ 
+ 5. Market Demands
+ Players determine Market Demands. Subsequently the next game round starts.
+
+ ---
+ 
+ # 🚂 Train Unit Data Table
+
+ | **ID** | **Name** | **Colour** | **Cost** | **Colour Code** | **Gen** | **Pool** | **Dice** |
+ |:------:|:----------------------------|:-----------|:--------:|:----------------:|:-------:|:--------:|:--------:|
+ | 1  | General I (4-2-1)          | Green  | 4  | 1 | 1 | 4 | 3 |
+ | 2  | Fast Freight I (8-4-2)     | Red    | 8  | 2 | 1 | 3 | 3 |
+ | 3  | Heavy I (12-6-3)           | Yellow | 12 | 3 | 1 | 2 | 2 |
+ | 4  | Special I (16-8-4)         | Blue   | 16 | 4 | 1 | 1 | 2 |
+ | 5  | General II (20-10-5)       | Green  | 20 | 1 | 2 | 4 | 4 |
+ | 6  | Fast Freight II (24-12-6)  | Red    | 24 | 2 | 2 | 3 | 3 |
+ | 7  | Heavy II (28-14-7)         | Yellow | 28 | 3 | 2 | 2 | 3 |
+ | 8  | General III (32-16-8)      | Green  | 32 | 1 | 3 | 4 | 4 |
+ | 9  | Special II (36-18-9)       | Blue   | 36 | 4 | 2 | 2 | 2 |
+ | 10 | Fast Freight III (40-20-10)| Red    | 40 | 2 | 3 | 3 | 4 |
+ | 11 | General IV (44-22-11)      | Green  | 44 | 1 | 4 | 4 | 4 |
+ | 12 | Heavy III (48-24-12)       | Yellow | 48 | 3 | 3 | 3 | 3 |
+ | 13 | Fast Freight IV (52-26-13) | Red    | 52 | 2 | 4 | 4 | 4 |
+ | 14 | General V (56-28-14)       | Green  | 56 | 1 | 5 | 4 | 5 |
+ ------------------------------------
+ */
+
+
 // MARK: Game Stages
 public enum GameStage: Int, CaseIterable, Codable {
     case idle
