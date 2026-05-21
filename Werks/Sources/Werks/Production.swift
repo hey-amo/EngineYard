d81d99a779f0aea978170a2cd6c6b60e50193489
AD <silentflute.ad@gmail.com>
2026-05-21 06:41:22 +0100

    updates

---
+++ Werks/Sources/Werks/Production.swift
@@ -0,0 +1,12 @@
+//
+//  Production.swift
+//  Werks
+//
+//  Created by Amarjit on 20/05/2026.
+//
+
+// Locomotive Train Production Handler
+public struct Production {
+    public let units: Int = 0
+    public let spentUnits: Int = 0
+}
