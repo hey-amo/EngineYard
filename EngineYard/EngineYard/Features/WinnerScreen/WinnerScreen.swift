d4a59a23b4dcfd81f187f0f4f0924fb1b3a58ecd
AD <silentflute.ad@gmail.com>
2026-06-08 14:33:57 +0100

    Adding quick and dirty art images

---
+++ EngineYard/EngineYard/Features/WinnerScreen/WinnerScreen.swift
@@ -0,0 +1,18 @@
+//
+//  WinnerScreen.swift
+//  EngineYard
+//
+//  Created by Amarjit on 08/06/2026.
+//
+
+import SwiftUI
+
+struct WinnerScreen: View {
+    var body: some View {
+        Text("Winner screen")
+    }
+}
+
+#Preview {
+    WinnerScreen()
+}
