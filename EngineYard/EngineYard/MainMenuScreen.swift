e775369b5419c2c354e177e3490ebd1440cc873f
AD <silentflute.ad@gmail.com>
2026-06-08 14:27:29 +0100

    Refactor: rename file to main menu screen

---
+++ EngineYard/EngineYard/MainMenuScreen.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  EngineYard
+//
+//  Created by Amarjit on 08/06/2026.
+//
+
+import SwiftUI
+
+struct MainMenuScreen: View {
+    
+    var body: some View {
+        Text("Main Menu")
+    }
+}
+
+#Preview {
+    MainMenuScreen()
+}
