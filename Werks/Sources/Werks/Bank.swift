a857715bc0328d35c86b0abcf231d44865a8a9f2
AD <silentflute.ad@gmail.com>
2026-06-28 23:43:52 +0100

    adding throws

--- Werks/Sources/Werks/Bank.swift
+++ Werks/Sources/Werks/Bank.swift
@@ -7,7 +7,9 @@
 
 /// To handle all payments
 public class Bank {
-    public func payTax(on amount: Int = 0) -> Int {
+    public func payTax(on amount: Int = 0) throws -> Int {
+        // Is it a valid number? : Throw validation error
+        // Can we afford it?: Throw validation error
         var amount = amount
         guard amount > 0 else { return amount }
         let sum = Tax.calculate(on: amount)
@@ -16,23 +18,31 @@ public class Bank {
     }
     
     // credit coins to a given player?
-    public func credit(_ amount: Int = 0) -> Int {
-        return 0
+    public func credit(_ amount: Int = 0) throws -> Int {
+        do {
+            let result: Bool = canCredit(amount)
+        } catch {   
+            fatalError("error thrown")
+        }
     }
     
     // debit coins from a given player?
     public func debit(_ amount: Int = 0) -> Int {
-        return 0
+        do {
+            let result: Bool = canDebit(amount)
+        } catch let err  : NumericValidator {
+            throw err
+        }
     }
     
     // MARK: Private functions
     
-    private func canCredit(_ amount: Int = 0) -> Bool {
+    private func canCredit(_ amount: Int = 0) throws -> Bool {
        // use try-catch to use the numberValidator to check amount is valid, throw if error
         return true
     }
     
-    private func canDebit(_ amount: Int = 0) -> Bool {
+    private func canDebit(_ amount: Int = 0) throws -> Bool {
         // use try-catch numberValidator only accept positiveNumber
         // use try-catch validateSufficientFunds only if this returns true
         // cannot debit a negative number
